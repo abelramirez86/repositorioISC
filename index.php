5528bcda941c8039281b2ba0dc47c8204d4ad943
aRamirezSota <abel.ramirez@us.edu.mx>
2024-08-27 17:10:47 -0600

    Ajuste de saltos de línea

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 include('abel.php');
+include('omar.php');
+include('Miguel.php');
+include('carlos.php');
+
 echo 'Hola Mundo';
 echo "<br>";
 echo abel();
+echo "<br>";
+echo omar();
+echo "<br>";
+echo Miguel();
+echo "<br>";
+echo carlos();
+
 ?>
